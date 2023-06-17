9b862249352cd803abb2d2e43de0da77dcbc7eee
mmitusov <maksym.mitusov@gmail.com>
2023-06-17 22:40:46 +0300

    Update

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+const IxCoach = 'https://www.ixcoach.com'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{width: '100%', height: '95%'}}>
+        <WebView 
+          source={{uri: IxCoach}}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+How to start the app and test-launch it on the smartphone:
+
+`npm i` - install dependancys
+`npm start` - start local server
+The QR code will show up
+Download `Expo Go` on you IOS or Android
+Launch `Expo Go` and scan QR code
+Done!
